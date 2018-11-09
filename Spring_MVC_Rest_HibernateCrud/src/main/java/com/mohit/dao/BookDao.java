17f66b4938532914bfbe2ab59915f9e6994e4ba8
Mohit Darmwal <36398566+mickmohit@users.noreply.github.com>
2018-11-09 13:11:25 +0530

    Add files via upload

---
+++ Spring_MVC_Rest_HibernateCrud/src/main/java/com/mohit/dao/BookDao.java
@@ -0,0 +1,15 @@
+package com.mohit.dao;
+
+import java.util.List;
+
+import com.mohit.model.Book;
+
+public interface BookDao {
+
+	   long save(Book book);
+	   Book get(long id);
+	   List<Book> list();
+	   void update(long id, Book book);
+	   void delete(long id);
+
+	}
